0f72c7b4c408be8229b83d5817454f9f005ccb30
FingerPrintGames <kevinjordan199449@gmail.com>
2022-07-22 07:26:28 -0400

    Add more styling to bug component

--- src/components/BugList/Bug.js
+++ src/components/BugList/Bug.js
@@ -4,19 +4,30 @@ import "../UI/Card.css";
 function Bug(props) {
   return (
     <div className="card bug">
-      <h2 className="bug-name">{props.bug.name}</h2>
-      <label className="priority-label">Priority: </label>
-      <p className="priority">{props.bug.priority}</p>
-      <h3 className="assigned">Assigned to: {props.bug.assignedTo}</h3>
-      <p className="description">{props.bug.description}</p>
-      <form>
-        <label htmlFor="status">Status:</label>
-        <select name="status" className="status">
-          <option value="Unresolved">Unresolved</option>
-          <option value="Fixes Underway">Fixes Underway</option>
-          <option value="Resolved">Resolved</option>
-        </select>
-        <button className="delete-bug">Delete</button>
+      <div className="top">
+        <h2 className="bug-name">{props.bug.name}</h2>
+        <div className="priority-div">
+          <label className="priority-label">Priority: </label>
+          <p className="priority">{props.bug.priority}</p>
+        </div>
+        <div className="member-div">
+          <label className="assigned-label">Assigned: </label>
+          <p className="assigned">{props.bug.assignedTo}</p>
+        </div>
+      </div>
+      <div className="middle">
+        <p className="description">{props.bug.description}</p>
+      </div>
+      <form className="bottom">
+        <div className="status-container">
+          <label className="status-label">Status:</label>
+          <select name="status" className="status">
+            <option value="Unresolved">Unresolved</option>
+            <option value="Fixes Underway">Fixes Underway</option>
+            <option value="Resolved">Resolved</option>
+          </select>
+        </div>
+        <button className="delete-bug">REMOVE</button>
       </form>
     </div>
   );
